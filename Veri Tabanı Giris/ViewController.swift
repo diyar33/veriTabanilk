87dc8c51a393aa018e9667cbf5372a2507828d05
Diyar Aydın <diyaraydin33@gmail.com>
2020-04-20 01:25:43 +0300

    Initial Commit

---
+++ Veri Tabanı Giris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Veri Tabanı Giris
+//
+//  Created by Ahmet Tarık Sürücü on 20.04.2020.
+//  Copyright © 2020 Diyar Aydın. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
